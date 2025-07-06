5668ac2a2be06164e708295302085c88a2d9c664
wxj512 <wxj512@york.ac.uk>
2025-07-06 23:29:45 +0100

    Minor changes for plotting, image filter test for edge detection for test.py

--- Misc/Test/Scripts/vel_out/test.py
+++ Misc/Test/Scripts/vel_out/test.py
@@ -1,12 +1,15 @@
 import os
 from tqdm import tqdm
 import numpy as np
+from scipy.ndimage import sobel
+from scipy.signal import find_peaks
+from skimage import feature
 from xbout import open_boutdataset
 import matplotlib.pyplot as plt
 
 
 def data_import(_):
-    folder = "delta_1_B0_" + "1.0"
+    folder = "delta_1_B0_" + "0.1"
     filepath = "../Data/Input/" + folder + "/"
     return filepath
 
@@ -26,10 +29,34 @@ BOUT_res = filepath + "BOUT.dmp.*.nc"
 ds = open_boutdataset(BOUT_res, info=False)
 ds = ds.squeeze(drop=True)
 
-# dx = ds["dx"].isel(x=0).values
-# ds = ds.drop_vars("x")
-# ds = ds.assign_coords(x=np.arange(ds.sizes["x"])*dx)
+dx = ds["dx"].isel(x=0).values
+ds = ds.drop_vars("x")
+ds = ds.assign_coords(x=np.arange(ds.sizes["x"])*dx)
 
-ds["n"].bout.animate2D()
+t = 50
+ds_data = ds.isel(t=t)
+
+fil = sobel(ds_data["n"].values - 1)
+fil2 = feature.canny(ds_data["n"].values - 1, sigma=0.1)
+
+
+col = 141
+fil = fil.transpose()
+fil2 = np.transpose(fil2)
+# line = fil[:, int(col)]
+line = fil2[:, int(col)]
+# print(line)
+# peaks, info = find_peaks(line, height=1e-04)
+f1 = plt.figure(1)
+ax1 = f1.gca()
+# ax1.plot(line)
+# ax1.set_xlim(0,256)
+ax1.imshow(fil, extent=[0,260,0,256])
+# ax1.scatter(peaks, line[peaks])
+f2= plt.figure(2)
+ax2 = f2.gca()
+ax2.imshow(fil2, extent=[0,260,0,256])
+ax2.vlines(col,0,256)
+
 
 plt.show()
--- Misc/Test/Scripts/vel_out/v_process.py
+++ Misc/Test/Scripts/vel_out/v_process.py
@@ -37,14 +37,16 @@ if __name__ == "__main__":
             ds = ds.assign_coords(x=np.arange(ds.sizes["x"])*dx)
 
             n_array_CoM = v_data.n_calc(ds)
-            n_array_nf = n_front.n_calc(ds, row_calc = "all_row")
+            n_array_nf = n_front.n_calc(ds)
+            n_array_nf_all = n_front.n_calc(ds, row_calc = "all_row")
             # n_array_FWHM = n_front_FWHM.n_calc(ds, row_calc = "all_row")
 
             dx_CoM, dz_CoM, vx_CoM, vz_CoM = v_data.vel_calc(n_array_CoM)
             dx_nf, dz_nf, vx_nf, vz_nf = v_data.vel_calc(n_array_nf)
+            dx_nf_all, dz_nf_all, vx_nf_all, vz_nf_all = v_data.vel_calc(n_array_nf_all)
             # dx_FWHM, dz_FWHM, vx_FWHM, vz_FWHM = v_data.vel_calc(n_array_FWHM)
 
-            max_v = np.array([[np.max(vx_CoM)], [np.max(vx_nf)]]).transpose()
+            max_v = np.array([[np.max(vx_CoM)], [np.max(vx_nf)], [np.max(vx_nf_all)]]).transpose()
 
             try:
                 max_v_array = np.append(max_v_array, max_v, axis = 0)
@@ -56,7 +58,8 @@ if __name__ == "__main__":
     f1 = plt.figure(linewidth = 3, edgecolor = "#000000")
     ax1 = f1.gca()
     ax1.plot(B0_data, max_v_array[:,0], label = "CoM method")
-    ax1.plot(B0_data, max_v_array[:,1], label = "n front method")
+    ax1.plot(B0_data, max_v_array[:,1], label = "n front method \nfor mid row")
+    ax1.plot(B0_data, max_v_array[:,2], label = "n front method \nfor all row")
     # ax1.plot(B0_data, max_v_array[:,2], label = "n front + FWHM method")
     ax1.set_xlabel("B0/T")
     ax1.set_ylabel("$v_{max}$/$c_s$")
