fd8b5a67aa73ff398849aca0bfe61d4981da3668
wxj512 <wxj512@york.ac.uk>
2025-07-08 13:07:06 +0100

    Added init.py for vel_out/

---
+++ Misc/Test/Scripts/vel_out/__init__.py
@@ -0,0 +1,8 @@
+# Define the __all__ variable
+__all__ = ["v_data", "max_n", "n_front_FWHM", "n_front"]
+
+# Import the submodules
+from . import v_data
+from . import max_n
+from . import n_front_FWHM
+from . import n_front
