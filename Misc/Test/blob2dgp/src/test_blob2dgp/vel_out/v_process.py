62f78d326b0a2009ad058c7b9134d963b5179f10
wxj512 <wxj512@york.ac.uk>
2025-07-14 06:37:43 +0100

    Wrapped all n_calc method as class and fixed FWHM method for curve fit failure at low B0 by lowering find_peaks height limit down to 0.005

--- Misc/Test/blob2dgp/src/test_blob2dgp/vel_out/v_process.py
+++ Misc/Test/blob2dgp/src/test_blob2dgp/vel_out/v_process.py
@@ -3,7 +3,7 @@ import numpy as np
 from xbout import open_boutdataset
 import matplotlib.pyplot as plt
 
-import v_data, n_front, n_front_FWHM 
+import v_data
 
 
 def main():  
@@ -24,14 +24,14 @@ def main():
             n_array_CoM = v_data.n_calc(ds, method = "CoM")
             n_array_nf = v_data.n_calc(ds, method = "n_front")
             # n_array_nf_all = n_front.n_calc(ds, row_calc = "all_row")
-            # n_array_FWHM = n_front_FWHM.n_calc(ds, row_calc = "all_row")
+            n_array_FWHM_all = v_data.n_calc(ds, method = "n_front_FWHM")
 
             dx_CoM, dz_CoM, vx_CoM, vz_CoM = v_data.vel_calc(n_array_CoM)
             dx_nf, dz_nf, vx_nf, vz_nf = v_data.vel_calc(n_array_nf)
             # dx_nf_all, dz_nf_all, vx_nf_all, vz_nf_all = v_data.vel_calc(n_array_nf_all)
-            # dx_FWHM, dz_FWHM, vx_FWHM, vz_FWHM = v_data.vel_calc(n_array_FWHM)
+            dx_FWHM_all, dz_FWHM_all, vx_FWHM_all, vz_FWHM_all = v_data.vel_calc(n_array_FWHM_all)
 
-            max_v = np.array([[np.max(vx_CoM)], [np.max(vx_nf)]]).transpose()
+            max_v = np.array([[np.max(vx_CoM)], [np.max(vx_nf)], [np.max(vx_FWHM_all)]]).transpose()
 
             try:
                 max_v_array = np.append(max_v_array, max_v, axis = 0)
@@ -45,7 +45,7 @@ def main():
     ax1.plot(B0_data, max_v_array[:,0], label = "CoM method")
     ax1.plot(B0_data, max_v_array[:,1], label = "n front method \nfor mid row")
     # ax1.plot(B0_data, max_v_array[:,2], label = "n front method \nfor all row")
-    # ax1.plot(B0_data, max_v_array[:,2], label = "n front + FWHM method")
+    ax1.plot(B0_data, max_v_array[:,2], label = "n front + FWHM method \nfor mid row")
     ax1.set_xlabel("B0/T")
     ax1.set_ylabel("$v_{max}$/$c_s$")
     ax1.legend()
