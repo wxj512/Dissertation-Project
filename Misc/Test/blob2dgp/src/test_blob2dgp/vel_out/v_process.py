1894c20db87fc7ca505263bdb3b25d1f8a43adc7
wxj512 <wxj512@york.ac.uk>
2025-07-14 17:26:10 +0100

    Added average velocity

--- Misc/Test/blob2dgp/src/test_blob2dgp/vel_out/v_process.py
+++ Misc/Test/blob2dgp/src/test_blob2dgp/vel_out/v_process.py
@@ -24,31 +24,47 @@ def main():
             n_array_CoM = v_data.n_calc(ds, method = "CoM")
             n_array_nf = v_data.n_calc(ds, method = "n_front")
             # n_array_nf_all = n_front.n_calc(ds, row_calc = "all_row")
-            n_array_FWHM_all = v_data.n_calc(ds, method = "n_front_FWHM")
+            # n_array_FWHM_all = v_data.n_calc(ds, method = "n_front_FWHM")
 
             dx_CoM, dz_CoM, vx_CoM, vz_CoM = v_data.vel_calc(n_array_CoM)
             dx_nf, dz_nf, vx_nf, vz_nf = v_data.vel_calc(n_array_nf)
             # dx_nf_all, dz_nf_all, vx_nf_all, vz_nf_all = v_data.vel_calc(n_array_nf_all)
-            dx_FWHM_all, dz_FWHM_all, vx_FWHM_all, vz_FWHM_all = v_data.vel_calc(n_array_FWHM_all)
+            # dx_FWHM_all, dz_FWHM_all, vx_FWHM_all, vz_FWHM_all = v_data.vel_calc(n_array_FWHM_all)
 
-            max_v = np.array([[np.max(vx_CoM)], [np.max(vx_nf)], [np.max(vx_FWHM_all)]]).transpose()
+            v_max = np.array([[np.max(vx_CoM)], [np.max(vx_nf)]]).transpose()
+            # v_avg = np.array([[np.mean(vx_CoM)], [np.mean(vx_nf)]]).transpose()
 
             try:
-                max_v_array = np.append(max_v_array, max_v, axis = 0)
-            except NameError: 
-                max_v_array = max_v
+                v_max_array = np.append(v_max_array, v_max, axis = 0)
+                # v_avg_array = np.append(v_avg_array, v_avg, axis = 0)
+            except NameError:
+                v_max_array = v_max
+                # v_avg_array = v_avg
+                
 
     B0_data = np.round(np.linspace(0.1,1,10),2)
 
     f1 = plt.figure(linewidth = 3, edgecolor = "#000000")
     ax1 = f1.gca()
-    ax1.plot(B0_data, max_v_array[:,0], label = "CoM method")
-    ax1.plot(B0_data, max_v_array[:,1], label = "n front method \nfor mid row")
+    ax1.set_title("Maximum velocity of blob for varying B0")
+    ax1.plot(B0_data, v_max_array[:,0], label = "CoM method")
+    ax1.plot(B0_data, v_max_array[:,1], label = "n front method \nfor mid row")
     # ax1.plot(B0_data, max_v_array[:,2], label = "n front method \nfor all row")
-    ax1.plot(B0_data, max_v_array[:,2], label = "n front + FWHM method \nfor mid row")
+    # ax1.plot(B0_data, max_v_array[:,2], label = "n front + FWHM method \nfor mid row")
     ax1.set_xlabel("B0/T")
     ax1.set_ylabel("$v_{max}$/$c_s$")
     ax1.legend()
+
+    # f2 = plt.figure(linewidth = 3, edgecolor = "#000000")
+    # ax2 = f2.gca()
+    # ax2.set_title("Average velocity of blob for varying B0")
+    # ax2.plot(B0_data, v_avg_array[:,0], label = "CoM method")
+    # ax2.plot(B0_data, v_avg_array[:,1], label = "n front method \nfor mid row")
+    # # ax2.plot(B0_data, max_v_array[:,2], label = "n front method \nfor all row")
+    # # ax2.plot(B0_data, max_v_array[:,2], label = "n front + FWHM method \nfor mid row")
+    # ax2.set_xlabel("B0/T")
+    # ax2.set_ylabel("$v_{avg}$/$c_s$")
+    # ax2.legend()
     plt.show()
 
 if __name__ == "__main__":
