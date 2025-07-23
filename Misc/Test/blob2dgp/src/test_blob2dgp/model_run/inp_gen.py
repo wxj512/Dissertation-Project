276cdc8fc83407a7b3c0840c43c6465dc963b931
wxj512 <wxj512@york.ac.uk>
2025-07-23 18:16:59 +0100

    Changed inp_gen.py to be compatible with epyscan latinhypercube or gridscan for generating inp files and folders

--- Misc/Test/blob2dgp/src/test_blob2dgp/model_run/inp_gen.py
+++ Misc/Test/blob2dgp/src/test_blob2dgp/model_run/inp_gen.py
@@ -4,9 +4,30 @@ import pathlib
 from xbout import open_boutdataset
 import fileinput
 import numpy as np
+import epyscan
 
-def mk_inp(param_array, param, ref_folder = "delta_1", path = ""):
-    ref_folder = ref_folder
+def param_gen(params: str, min_max, n_samples = None, log = None, endpoint = None):
+## Parameter generation for epyscan gridscan or latinhypercube
+
+## min_max in the form of np array:
+## For param a,b: min_max = [[min(a), max(a)], [min(b), max(b)]]
+    parameters = {}
+    for i, p_val in enumerate(params):
+        parameters.update({
+            p_val: {"min": min_max[i][0], "max": min_max[i][1]}
+        })
+
+        if n_samples is not None and isinstance(n_samples[i], int) == True:
+            parameters[p_val].update({"n_samples": n_samples[i]})
+
+        if log is not None and log[i] == True:
+            parameters[p_val].update({"log": True})
+
+        if endpoint is not None and endpoint[i] == False:
+            parameters[p_val].update({"endpoint": False})
+    return parameters
+
+def mk_inp(param_array, params, ref_folder = "delta_1", path = "", campaign_no = 1):
 
     if not(path == ""):
         run_path = path
@@ -17,20 +38,24 @@ def mk_inp(param_array, param, ref_folder = "delta_1", path = ""):
         run_path = build_path.joinpath("Builds", build,"examples/blob2d")
 
     for i, val in enumerate(param_array):
-
-        folder = f"delta_1_B0_{val:.1f}"
-        filepath = run_path.joinpath(folder)
+        campaign = f"campaign_{campaign_no}"
+        folder = ["delta_1"] + [f"{t[0]}_" + f"{t[1]:.1f}" for t in val.items()] 
+        folder = "_".join(folder)
+        filepath = run_path.joinpath(campaign, folder)
         
-        if not(os.path.exists(filepath) and os.path.isdir(filepath)):
+        if not(os.path.exists(filepath.parent) and os.path.isdir(filepath.parent)):
+            os.mkdir(filepath.parent)
+        elif not(os.path.exists(filepath) and os.path.isdir(filepath)):
             os.mkdir(filepath)
 
-        BOUT_res = filepath.parent.joinpath(ref_folder, "BOUT.dmp.*.nc")
-        BOUT_inp = filepath.parent.joinpath(ref_folder, "BOUT.inp")
+        BOUT_res = filepath.parents[1].joinpath(ref_folder, "BOUT.dmp.*.nc")
+        BOUT_inp = filepath.parents[1].joinpath(ref_folder, "BOUT.inp")
         
         try:
             ds_inp = open_boutdataset(BOUT_res, inputfilepath=BOUT_inp, info=False)
             inp_txt = ds_inp.attrs["options"]
-            inp_txt["model"][param] = f"{val:.1f}"
+            for p_var in params:
+                inp_txt["model"][p_var] = f"{val[p_var]:.1f}"
             inp_file = filepath.joinpath("BOUT.inp")
             pathlib.Path.write_text(inp_file,str(inp_txt))
         except OSError:
@@ -40,11 +65,19 @@ def mk_inp(param_array, param, ref_folder = "delta_1", path = ""):
                 inp_new.seek(0)
 
                 for j, line in enumerate(inp):
-                    if param in line:
+                    if params in line:
                         param_val = f"{val:.1f}"
                         line = re.sub(r"(=) (\d*\.*\d+)","= " + param_val, line)
                     inp_new.write(line)
 
+def main():
+    params = ["B0", "Te0"]
+    min_max = np.array([[0.1, 1.0], [11, 20]])
+    n_samples = [10, 10]
+    parameters = param_gen(params, min_max, n_samples = n_samples)
+    grid_scan = epyscan.GridScan(parameters)
+    mk_inp(grid_scan, params)
+
+
 if __name__ == "__main__":
-    B0_inp = np.linspace(1.1, 2, 10)
-    mk_inp(B0_inp, "B0")
+    main()
