49675d7da5a392a056a67c90c8ee987fff757003
wxj512 <wxj512@york.ac.uk>
2025-07-23 22:06:05 +0100

    Test using pathlib.glob for looping folders/files in folder

--- Misc/Test/blob2dgp/src/test_blob2dgp/model_run/test.py
+++ Misc/Test/blob2dgp/src/test_blob2dgp/model_run/test.py
@@ -1,21 +1,13 @@
 import pathlib
 from xbout import open_boutdataset
 
-ref_folder = "delta_1"
-
 build_path = pathlib.Path(__file__).parents[8]
 build = "BOUT++5.1.1"
+# path = "../../../../../Builds/BOUT++5.1.1/examples/blob2d/"
 run_path = build_path.joinpath("Builds", build,"examples/blob2d")
-
-folder = f"delta_1_B0_1.1"
-filepath = run_path.joinpath(folder)
-
-BOUT_res = filepath.parent.joinpath(ref_folder, "BOUT.dmp.*.nc")
-BOUT_inp = filepath.parent.joinpath(ref_folder, "BOUT.inp")
-val=1.1220
-ds_inp = open_boutdataset(BOUT_res, inputfilepath=BOUT_inp, info=False)
-file_txt = ds_inp.attrs["options"]
-file_txt["model"]["B0"]=f"{val:.1f}"
-file = filepath.joinpath("BOUT.inp")
-pathlib.Path.write_text(file,str(file_txt))
-print(file_txt)
+camp = 1
+camp_path = run_path.joinpath(f"campaign_{camp}")
+f = camp_path.glob("*")
+folders = []
+folders = [str(obj) for obj in camp_path.glob("*")]
+print(folders)
