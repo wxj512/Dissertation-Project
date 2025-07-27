c157f3c6b895923648b5bcb5a0d933b54f847b41
wxj512 <wxj512@york.ac.uk>
2025-07-28 00:30:24 +0100

    v_process now outputs .nc file according to parameters specified

--- Misc/Test/blob2dgp/src/test_blob2dgp/vel_out/test.py
+++ Misc/Test/blob2dgp/src/test_blob2dgp/vel_out/test.py
@@ -8,26 +8,52 @@ from scipy.signal import find_peaks
 from scipy.optimize import curve_fit
 from tqdm import tqdm
 import xarray as xr
+from operator import itemgetter
 
 import v_data
 
 
-BOUT_res, BOUT_settings = v_data.data_import(folder = "delta_1")[0:2]
+len = 5*4*3*2
+arr = np.linspace(1, len, len)
 
-ds = open_boutdataset(BOUT_res, inputfilepath = BOUT_settings, info=False)
-ds = ds.squeeze(drop=True)
+arr = np.reshape(arr, [5, 4, 3, 2])
+g = ["a", "b", "c", "d"]
+q = ["c", "d"]
 
-dx = ds["dx"].isel(x=0).values
-ds = ds.drop_vars("x")
-ds = ds.assign_coords(x=np.arange(ds.sizes["x"])*dx)
-param_var = ["B0", "Te0", "n0", "R_c"]
-params = {}
-param_val = np.empty((0, 4))
-param_val = np.array([[ds.attrs["options"]["model"][var]] for var in param_var]).transpose()
-[params.update({f"{var}": ds.attrs["options"]["model"][var]}) for var in param_var]
-a = np.array([[1], [2], [3], [4]]).transpose()
-param_val = np.append(param_val, a, axis = 0)
+dims = {
+    "a": [1, 2, 3, 4, 5],
+    "b": [6, 7, 8, 9],
+    "c": [1, 1, 1],
+    "d": [2, 2]
+}
 
-data_path = v_data.data_import("")[3]
-data_input_path = data_path.joinpath("Input", f"campaign_{1}")
-print(len(list(data_input_path.glob("*/"))))
+dims_attrs = {
+    "a": {"long_name": "coord a"},
+    "b": {"long_name": "coord b"},
+    "c": {"long_name": "coord c"},
+    "d": {"long_name": "coord d"}
+}
+
+coords = dict(
+        [(var, (var, dims[var], dims_attrs[var])) for var in g]
+)
+
+da_1 = xr.DataArray(arr, coords = dims, attrs={"long_name": "array 1"})
+da_2 = xr.DataArray(arr, coords = dims, attrs={"long_name": "array 2"})
+# coords = xr.DataArray(dims, attrs = dims_attrs)
+
+ds = xr.Dataset(
+    data_vars = {
+    "arr_1": da_1,
+    "arr_2": da_2,
+
+    },
+
+    coords = coords
+
+)
+# [print(val) for val in dims.values()]
+# reshape_list = [np.unique(dims[var].values()).size for var in g if np.unique(dims[var].values()).size != 1]
+# print(reshape_list)
+# [dims.pop(var, None) for var in list(set(g)-set(q))]
+print(itemgetter("a", "b")(dims))
