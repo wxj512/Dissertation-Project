5249391277efb5c35d6bdc6bbee919995b965026
wxj512 <wxj512@york.ac.uk>
2025-07-28 08:59:32 +0100

    Rewrote v_all.nc for campaign 1 due to sorting error, and test for reading the .nc file

--- Misc/Test/blob2dgp/src/test_blob2dgp/vel_out/test.py
+++ Misc/Test/blob2dgp/src/test_blob2dgp/vel_out/test.py
@@ -13,31 +13,19 @@ from operator import itemgetter
 import v_data
 
 
-BOUT_res, BOUT_settings = v_data.data_import(folder = "campaign_1/delta_1_B0_0.1_Te0_4.0")[0:2]
+ax = np.array([[1,1,2,2,3,3,4,4,5,5], [1,2,1,2,1,2,1,2,1,2]]).transpose()
+a = np.array([[2,2,2], [3,3,3], [5,5,5], [1,1,1], [4,4,4], [8,8,8], [6,6,6], [7,7,7], [9,9,9], [10,10,10]])
+np.random.shuffle(ax)
 
-ds = open_boutdataset(BOUT_res, inputfilepath=BOUT_settings, info=False)
-ds = ds.squeeze(drop=True)
-dx = ds["dx"].isel(x=0).values
-ds = ds.drop_vars("x")
-ds = ds.assign_coords(x=np.arange(ds.sizes["x"])*dx)
-t = 47
-ds_data = ds.isel(t=t)
+temp = np.empty((0,10))
+temp = [np.append(temp, ax[:,col]) for col in np.arange(ax.shape[1])[::-1]]
 
-front, row, gauss_fit = v_data.n_calc_methods(ds_data, "all_row", 1, 0.3, "n_front_FWHM")
-nf_front, row_j = v_data.n_calc_methods(ds_data, "all_row", 1, 0.3, "n_front")
+print(temp)
+ax_sort = np.lexsort(temp,axis=0)
 
-n = ds_data["n"].values[:,row]-1
+print(ax)
+ax = ax[ax_sort]
+print(ax)
 
-f1 = plt.figure(1, linewidth = 3, edgecolor = "#000000")
-ax1 = f1.gca()
-ax1.set_title("z/$\\rho_s$ = " + str(np.round(ds["z"].values[row_j],1)) + ", t/(1/$\\Omega_i$) = " + str(t*500))
-ax1.plot(ds_data["x"], n,linewidth=0.8, label="Density profile")
-ax1.plot(ds_data["x"], gauss_fit, linewidth=0.8, linestyle = "-.", label="Gaussian fit of last peak")
-ax1.scatter(nf_front[0],n[int(np.where(ds_data["x"]==nf_front[0])[0][0])],marker="x", color="orange", label="Peak position")
-ax1.vlines(front[0], np.min(n)-0.05, np.max(n)+0.1, linestyle = "--", linewidth=0.5, color="black", label = "Position of \nright side of FWHM")
-ax1.set_ylim(np.min(n)-0.05, np.max(n)+0.1)
-ax1.set_xlabel("x/$\\rho_s$")
-ax1.set_ylabel("Density/(n/$n_0$)")
-ax1.legend(fontsize="small")
 
-plt.show()
+
