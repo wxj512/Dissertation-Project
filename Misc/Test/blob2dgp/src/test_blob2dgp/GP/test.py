4f73886392d71dd5bb240136383110bbf0de908c
wxj512 <wxj512@york.ac.uk>
2025-07-28 08:59:32 +0100

    Rewrote v_all.nc for campaign 1 due to sorting error, and test for reading the .nc file

--- Misc/Test/blob2dgp/src/test_blob2dgp/GP/test.py
+++ Misc/Test/blob2dgp/src/test_blob2dgp/GP/test.py
@@ -27,14 +27,17 @@ def main():
     # training_indices = rng.choice(np.arange(ds_y.size), size = int(0.6 * ds_y.size), replace = False)
     # x_train, y_train = ds_x[training_indices], ds_y[training_indices]
 
-    
-    vall_data_CoM = vall_ds["v_max"].sel(n_method = "CoM", Te0 = 12)
-    vall_data_nf = vall_ds["v_max"].sel(n_method = "n_front_all", Te0 = 12)
-    vall_data_FWHM = vall_ds["v_max"].sel(n_method = "FWHM_all", Te0 = 12)
-    vall_data_CoM.plot()
-    vall_data_nf.plot()
-    vall_data_FWHM.plot()
+    # Te0 = 40
+    # vall_data_CoM = vall_ds["v_max"].sel(n_method = "CoM", Te0 = Te0)
+    # vall_data_nf = vall_ds["v_max"].sel(n_method = "n_front_all", Te0 = Te0)
+    # vall_data_FWHM = vall_ds["v_max"].sel(n_method = "FWHM_all", Te0 = Te0)
+    # vall_data_CoM.plot()
+    # vall_data_nf.plot()
+    # vall_data_FWHM.plot()
+    vmax_CoM = vall_ds["v_max"].sel(n_method = "CoM")
+    vmax_CoM.plot(x="B0", y="Te0")
     plt.show()
+    # print(vall_ds)
 
 if __name__ == "__main__":
     main()
