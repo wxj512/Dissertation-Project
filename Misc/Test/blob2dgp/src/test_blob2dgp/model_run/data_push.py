c40dcceb65e604656db1176d25c7c4c0258fcf19
wxj512 <wxj512@york.ac.uk>
2025-08-06 00:45:18 +0100

    Modified data_push.py to remove magic numbers

--- Misc/Test/blob2dgp/src/test_blob2dgp/model_run/data_push.py
+++ Misc/Test/blob2dgp/src/test_blob2dgp/model_run/data_push.py
@@ -3,29 +3,16 @@ from tqdm import tqdm
 
 import test_blob2dgp.vel_out.v_data as v_data
 
-
-
-def git_push(repo_path):
-    try:
-        repo = Repo(repo_path)
-        repo.git.add(update=True)
-        repo.index.commit("COMMIT_MESSAGE")
-        origin = repo.remote(name = "main")
-        origin.push()
-    except:
-        print('Some error occured while pushing the code')    
-
-
 def main():
     data_path = v_data.data_import("")[3]
+    repo_path = data_path.parents[2]
+    campaign_path = data_path.joinpath("Input", f"campaign_{2}")
 
-    repo_path = data_path.joinpath("Input", "campaign_2")
-
-    folder_list = [folder.name for folder in repo_path.glob("*/")]
+    folder_list = [folder.name for folder in campaign_path.glob("*/")]
     folder_i = 0
     batch = 0
 
-    repo = Repo(repo_path.parents[4])
+    repo = Repo(repo_path)
     main = repo.remote(name = "origin")
     
     for folder in tqdm(folder_list):
@@ -33,7 +20,7 @@ def main():
         folder_path = r"Misc/Test/Data/Input/campaign_2/"
         folder_path = folder_path + folder + "/."
         repo.git.add(folder_path)
-        if folder_i % 5 == 0 or folder_i == 400:
+        if folder_i % 5 == 0 or folder_i == len(folder_list):
             batch += 1
             repo.index.commit(f"Adding campaign_2 folder batch {batch}")
             main.push()
